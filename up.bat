f819ca5dde47d06e578e3f9c9db296ec5d8b528e
i3z1 <97494157+i3z1@users.noreply.github.com>
2024-11-04 12:13:43 +0300

    first commit

--- up.bat
+++ up.bat
@@ -3,5 +3,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/i3z1/happybirthday.git
+git remote add origin https://github.com/i3z1/happybirthday1.git
 git push -u origin main
